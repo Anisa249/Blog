11148e4b9ed2ce1b7a387e9eeb8f5b3865e526c0
Anisa249 <58497954+Anisa249@users.noreply.github.com>
2020-07-09 13:48:11 +0530

    Update comment_post.php

--- comment_post.php
+++ comment_post.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html> 
-<<<<<<< HEAD
 <head>
 	<meta http-equiv="refresh" content="1; url=comments.php" /> 
 	<title></title> 
@@ -9,23 +8,15 @@
 $name=$_POST['name'];
 $comment=$_POST['comment']; 
 $hostname="localhost";
-=======
-<head><meta http-equiv="refresh" content="1; url=index.php" /> <title></title> </head> <body> </body> </html> 
-<?php $name=$_POST['name'];
-$comment=$_POST['comment']; $hostname="localhost";
->>>>>>> cd847aff1045d80b967066ba60ba219aa76730af
 $username="root";
 $password="";
 $db="comments";
 $conn=mysqli_connect($hostname, "root", "", $db); 
 $sql="INSERT INTO comments(name,comment) VALUES ('$name','$comment');";    //default password for mamp is "root" and for xampp is empty 
       $query=mysqli_query($conn,$sql); 
-<<<<<<< HEAD
+
 if(!$query) {
      echo "could not insert into database <br>";
-=======
-if(!$query) {     echo "could not insert into database <br>";
->>>>>>> cd847aff1045d80b967066ba60ba219aa76730af
 }
 else {
     echo " Sucessfully inserted inside database"; }
